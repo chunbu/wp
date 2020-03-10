739dbe8c789f305fd3f5f327d542ae1bf820c97d
chunbu <@gmail.com>
2020-03-10 17:25:12 +0700

    lúp configs

--- wp-content/plugins/mb-personalized/include/personalized.php
+++ wp-content/plugins/mb-personalized/include/personalized.php
@@ -18,87 +18,78 @@ class MB_Personalized{
         
         add_action( 'woocommerce_checkout_create_order_line_item', array( $this,'print_data_add_to_bill'), 10, 4);
 
-
-        add_action( 'init', function(){
-            if ( isset( $_GET['dev'] ) ) {
-                $option = get_option( 'order-value' );
-                echo '<pre>';
-                print_r( $option );
-                echo '</pre>';
-            }
-        });
+       
     }
-    function get_data_json() {
 
-        $string1 = file_get_contents("https://gist.githubusercontent.com/TruongTuyen/d8c066d61c71da1f992713bc2e02fe2c/raw/4bc27e6e6e6df2bf51955c26b0fc10d038bbaad7/persionalized-configs.json");
-        $json = json_decode($string1, true);
-        foreach ($json as $key => $value) {
-            ?>
-            <div style="width:300px">
-             <?php if ($value['field_type']==='text'||$value['field_type']==='number') { ?>
+    function render_personalize_fields( $configs = array() ) {
+        if ( is_array( $configs ) && ! empty( $configs ) ) {
+            foreach ( $configs as $key => $value) {
+                ?>
+                <div style="width:300px">
+                 <?php if ($value['field_type']==='text'||$value['field_type']==='number') { ?>
+                        <div class="<?php echo esc_attr($value['title'])?>">
+                            <label for=""><?php echo esc_html($value['title'])?></label>:<br>
+                            <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" placeholder="<?php echo esc_attr($value['title'])?>" style="width:300px" <?php if(!empty($value['configs']['settings']['min'])||!empty($value['configs']['settings']['max'])){ $a = $value['configs']['settings']['min']; $b=$value['configs']['settings']['max'];echo "min='$a'max='$b'";}?>>
+                        </div>
+                <?php } elseif($value['field_type']==='textarea'){ ?>
                     <div class="<?php echo esc_attr($value['title'])?>">
                         <label for=""><?php echo esc_html($value['title'])?></label>:<br>
-                        <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" placeholder="<?php echo esc_attr($value['title'])?>" style="width:300px" <?php if(!empty($value['configs']['settings']['min'])||!empty($value['configs']['settings']['max'])){ $a = $value['configs']['settings']['min']; $b=$value['configs']['settings']['max'];echo "min='$a'max='$b'";}?>>
+                        <textarea class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]"></textarea>
                     </div>
-            <?php } elseif($value['field_type']==='textarea'){ ?>
-                <div class="<?php echo esc_attr($value['title'])?>">
+                <?php } elseif($value['field_type']==='checkbox'){ ?>
+                    <div class="<?php echo esc_attr($value['title'])?>" style="text-align:justify">
                     <label for=""><?php echo esc_html($value['title'])?></label>:<br>
-                    <textarea class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]"></textarea>
-                </div>
-            <?php } elseif($value['field_type']==='checkbox'){ ?>
-                <div class="<?php echo esc_attr($value['title'])?>" style="text-align:justify">
-                <label for=""><?php echo esc_html($value['title'])?></label>:<br>
-                <?php foreach($value['configs']['settings']['options'] as $key => $val){
-                    ?>
-                        <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>][]" placeholder="<?php echo esc_attr($value['title'])?>" <?php if($val==='Orange'){ echo 'checked';}?> value="<?php echo esc_attr($val)?>">
-                        <span ><?php echo esc_html($val)?></span>
-                    <?php
-                    }
-                ?>
-                </div>
-            <?php } elseif($value['field_type']==='radio'){ ?>
-                <div class="<?php echo esc_attr($value['title'])?>" style="text-align:justify">
-                    <label for=""><?php echo esc_html($value['title'])?></label>:<br>
-                    <div>
-                        <?php foreach($value['configs']['settings']['options'] as $key => $val){
-                            ?>
-                            <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" placeholder="<?php echo esc_attr($value['title'])?>" <?php if($val==='Orange'){ echo 'checked = checked';}?> value="<?php echo esc_attr($val)?>">
-                            <span><?php echo esc_html($val)?></span>
+                    <?php foreach($value['configs']['settings']['options'] as $key => $val){
+                        ?>
+                            <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>][]" placeholder="<?php echo esc_attr($value['title'])?>" <?php if($val==='Orange'){ echo 'checked';}?> value="<?php echo esc_attr($val)?>">
+                            <span ><?php echo esc_html($val)?></span>
                         <?php
                         }
-                       ?>
+                    ?>
                     </div>
-                </div>
-            <?php } else { ?>
-                <div class="<?php echo esc_attr($value['title'])?>">
-                    <label for=""><?php echo esc_html($value['title'])?></label>:<br>
-                    <select name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" style="width:300px">
-                        <?php foreach($value['configs']['settings']['options'] as $key => $val){
-                            ?>
-                              <br>
-                                <option value="<?php echo esc_attr($val['value'])?>"><?php echo esc_html( $val['label'])?></option>
+                <?php } elseif($value['field_type']==='radio'){ ?>
+                    <div class="<?php echo esc_attr($value['title'])?>" style="text-align:justify">
+                        <label for=""><?php echo esc_html($value['title'])?></label>:<br>
+                        <div>
+                            <?php foreach($value['configs']['settings']['options'] as $key => $val){
+                                ?>
+                                <input type="<?php echo esc_attr($value['field_type'])?>"class="<?php echo esc_attr($value['field_name'])?>" id="<?php echo esc_attr($value['field_name'])?>" name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" placeholder="<?php echo esc_attr($value['title'])?>" <?php if($val==='Orange'){ echo 'checked = checked';}?> value="<?php echo esc_attr($val)?>">
+                                <span><?php echo esc_html($val)?></span>
                             <?php
-                        }
-                       ?>
-                    </select>
-                </div>
-            <?php } ?> 
-        </div>
-        <br>
-        <?php
+                            }
+                           ?>
+                        </div>
+                    </div>
+                <?php } else { ?>
+                    <div class="<?php echo esc_attr($value['title'])?>">
+                        <label for=""><?php echo esc_html($value['title'])?></label>:<br>
+                        <select name="pm_personalized[<?php echo esc_attr($value['field_name'])?>]" style="width:300px">
+                            <?php foreach($value['configs']['settings']['options'] as $key => $val){
+                                ?>
+                                  <br>
+                                    <option value="<?php echo esc_attr($val['value'])?>"><?php echo esc_html( $val['label'])?></option>
+                                <?php
+                            }
+                           ?>
+                        </select>
+                    </div>
+                <?php } ?> 
+            </div>
+            <?php
+            }
         }
-        global $post;
-        $product = wc_get_product($post->ID);
-        $title = $product->get_meta('custom-field-title');
-        // if($title){
-            printf(
-                '<div class="custom-field-title">
-                    <label for="field-title">%s :</label><br>
-                    <input type="text" name="pm_personalized[custom-field-title]" id="custom-field-title" placeholder="You Title" style="width:300px">
-                </div><br>',
-                esc_html($title)
-            );
-        //}
+    }
+    function get_data_json() {
+        global $product;
+
+        if ( is_object( $product ) && method_exists( $product, 'get_id' ) ) {
+            $product_id = $product->get_id();
+            if ( is_numeric( $product_id ) && $product_id > 0 ) {
+                $personalized_configs = get_post_meta( $product_id, '_mb_personalized_configs', true );
+
+                $this->render_personalize_fields( $personalized_configs );
+            }
+        }
     }
 
     function add_display_custom_field_back_end(){
@@ -120,94 +111,29 @@ class MB_Personalized{
     }
 
     function validate_custom_field($passed, $post_id, $quantity){
-        if( empty( $_POST['cfwc-title-field'] ) ) {
-            // Fails validation
-            $passed = false;
-            wc_add_notice( __( 'Please enter a value into the text field', 'cfwc' ), 'error' );
-        }
-        return $passed;
         
-        // if(empty($_POST['your-name'])){
-        //     $passed = false;
-        //     wc_add_notice(__(' Please enter a value into the your-name. ', 'cfwc'), 'error');
-    
-        // }
-        // if(empty($_POST['your-quotes'])){
-        //     $passed = false;
-        //     wc_add_notice(__(' Please enter a value into the your-quotes. ', 'cfwc'), 'error');
-    
-        // }
-        // if(empty($_POST['your-number'])){
-        //     $passed = false;
-        //     wc_add_notice(__(' Please enter a value into the your-number. ', 'cfwc'), 'error');
-    
-        // }
-        // if(empty($_POST['your-checkbox'])){
-        //     $passed = false;
-        //     wc_add_notice(__(' Please enter a value into the your-checkbox. ', 'cfwc'), 'error');
-    
-        // }if(empty($_POST['your-radio'])){
-        //     $passed = false;
-        //     wc_add_notice(__(' Please enter a value into the your-radio. ', 'cfwc'), 'error');
-    
-        // }
-        // // if(empty($_POST['your-dropdown'])){
-        // //     $passed = false;
-        // //     wc_add_notice(__(' Please enter a value into the your-dropdown. ', 'cfwc'), 'error');
-    
-        // // }
-        // // if(empty($_POST['title'])){
-        // //     $passed = false;
-        // //     wc_add_notice(__(' Please enter a value into the title. ', 'cfwc'), 'error');
-    
-        // // }
-        // return $passed;
     }
 
     function pin_data_add_to_cart($cart_item_data, $product_id, $variation_id, $quantity){
         
-        if( ! empty( $_POST) ) {
-            // Add the item data
-            $ignore_keys = array( 'quantity', 'add-to-cart' );
-            $personalized_data = array();
-            foreach($_POST as $key => $value){
-                if ( ! in_array( $key, $ignore_keys ) ) {
-                    $personalized_data[ $key ] = $value;
-                }
-            }
-            
-            if ( ! empty( $personalized_data ) ) {
-                $cart_item_data['personalized_data'] = $personalized_data;
-            }
-            
+        foreach($_POST as $key => $value){
+            $cart_item_data[$key]= $value;
         }
-        echo '<pre>';
-                print_r( $_POST );
-                echo '</pre>';
+       
         return $cart_item_data;
     }
-    function print_data_add_to_cart($item_data, $cart_item_data ){
 
-        $data = $cart_item_data['personalized_data'];
+    function print_data_add_to_cart($item_data, $cart_item_data ){
+        
+        $data = $cart_item_data['pm_personalized'];
         foreach ($data as $key => $value) {
             if (is_array($value)) {
-               foreach($value as $k => $val){
-                    if (is_array($val)) {
-                        foreach ($val as $m => $v) {
-                            $item_data[$m] = array(
-                                'key' => __($k),
-                                'value' => wc_clean($v ),
-                                'display' => '',
-                            );
-                        }
-                    }else{
-                        $item_data[$k] = array(
-                            'key' => __( $k),
-                            'value' => wc_clean( $val ),
-                            'display' => '',
-                        );
-                    }
-               }
+                $data = implode( ', ', $value );
+                $item_data[$keys] = array(
+                    'key' => __( $key),
+                    'value' => wc_clean( $data ),
+                    'display' => '',
+                );
             }
             else{
                 $item_data[$key] = array(
@@ -221,17 +147,20 @@ class MB_Personalized{
 
         return $item_data;
     }
+    
     function print_data_add_to_bill(  $item, $item_data, $values, $order ){
-       
-        
 
-        update_option('order-item', $item);
-       
-        update_option('order12e',$item_data);
+        if ( is_array( $values['pm_personalized'] ) && ! empty( $values['pm_personalized'] ) ) {
+            update_option('update_otion_item', $values['pm_personalized']);
+            foreach( $values['pm_personalized'] as $key => $value ){
+                $data = $value;
+                if ( is_array( $value ) ) {
+                    $data = implode( ', ', $value );
+                }
+                $item->add_meta_data( $key, $data, true );
 
-        update_option('order-value',$values);
-     
-        //$item->add_meta_data( __( 'Custom Field', 'cfwc' ), 'híhihd', true );
+            }
+        }
     }
     
 }
